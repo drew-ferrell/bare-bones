ec13b9f5cd72dac4da500ccdf2a4a1410ee40bb8
drew-ferrell <65027801+drew-ferrell@users.noreply.github.com>
2020-05-10 11:39:01 -0500

    Comment lines.

--- example.py
+++ example.py
@@ -1,6 +1,10 @@
 from structures import Struct
 from enrichment import G
+# link to Gene Ontology annotations.
 link = 'https://www.arabidopsis.org/download_files/GO_and_PO_Annotations/Gene_Ontology_Annotations/ATH_GO_GOSLIM.txt'
+# place file name(s) containing genes for testing.
 test_list = ['gene_list.tsv']
+# implement the Struct class.
 struct = Struct(t = test_list, l = link)
+# conduct Gene Ontology enrichment with significance cut- off.
 go = G(struct, 0.05)
